c9a2f2370adb1088bb65d5dab061eae694636ddf
Pasconsa <saulopascon@hotmail.com>
2022-11-15 18:43:13 -0300

    02.2Atualizando o registro (logica para atualizar registro)

--- src/main/java/br/com/colecao/games/GamesApplication.java
+++ src/main/java/br/com/colecao/games/GamesApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.colecao.games.orm.Console;
-import br.com.colecao.games.repository.ConsoleRepository;
 import br.com.colecao.games.service.CrudConsoleService;
 
 @SpringBootApplication
--- src/main/java/br/com/colecao/games/service/CrudConsoleService.java
+++ src/main/java/br/com/colecao/games/service/CrudConsoleService.java
@@ -1,5 +1,6 @@
 package br.com.colecao.games.service;
 
+
 import java.util.Scanner;
 
 import org.springframework.stereotype.Service;
@@ -10,24 +11,60 @@ import br.com.colecao.games.repository.ConsoleRepository;
 @Service
 public class CrudConsoleService {
 
+	private Boolean system = true;
 	private final ConsoleRepository consoleRepository;
 
 	public CrudConsoleService(ConsoleRepository consoleRepository) {
 		this.consoleRepository = consoleRepository;
 	}
-		
+
 	public void inicial(Scanner scanner) {
-		salvar(scanner);
+		while (system) {
+			System.out.println("Qual acao Console executar");
+			System.out.println("0 - sair");
+			System.out.println("1 - Salvar");
+			System.out.println("2 - Atualizar");
+
+			int acao = scanner.nextInt();
+
+			switch (acao) {
+			case 1:
+				salvar(scanner);
+				break;
+			case 2:
+				atualizar(scanner);
+				break;
+			default:
+				system = false;
+				break;
+			}
+		}
+
 	}
-	
-	
-	public void salvar (Scanner scanner) {
-		System.out.println("Nome do Console");
-		String nome = scanner.next();
+
+	public void salvar(Scanner scanner) {
+		System.out.println("nome console");
+		String nome = scanner.nextLine().toUpperCase();     //UpperCase para ficar padrão a forma que é salvo.
+        nome += scanner.nextLine().toUpperCase();
 		Console console = new Console();
 		console.setNome(nome);
 		consoleRepository.save(console);
 		System.out.println("Salvo");
 	}
+
+	public void atualizar(Scanner scanner) {
+		System.out.println("id");
+		int id = scanner.nextInt();
+		System.out.println("Nome do console");
+		String nome = scanner.nextLine().toUpperCase();     //UpperCase para ficar padrão a forma que é salvo.
+        nome += scanner.nextLine().toUpperCase();
 		
+		Console console = new Console();
+		console.setId(id);
+		console.setNome(nome);
+		consoleRepository.save(console);
+		System.out.println("Atualizado");
+
+	}
+
 }
