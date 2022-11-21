1dacbdaae05fa73d75b81abbfd84ca939e95687a
Pasconsa <saulopascon@hotmail.com>
2022-11-21 12:19:14 -0300

    03.2 JPQL query pesquisa Game de nome , produtora e ano lancamento

--- src/main/java/br/com/colecao/games/repository/GameRepository.java
+++ src/main/java/br/com/colecao/games/repository/GameRepository.java
@@ -2,10 +2,14 @@ package br.com.colecao.games.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.colecao.games.orm.Game;
 
 public interface GameRepository extends CrudRepository<Game, Integer> {
 List<Game> findByNome(String nome);
+
+@Query("SELECT g FROM Game g WHERE g.nome = :nome AND g.produtora = :produtora AND g.anoLancamento >= :anoLancamento")
+List <Game> findByNomeProdutoraAnoLancamentoGames (String nome, String produtora, int anoLancamento);
 }
--- src/main/java/br/com/colecao/games/service/RelatorioService.java
+++ src/main/java/br/com/colecao/games/service/RelatorioService.java
@@ -24,13 +24,17 @@ public class RelatorioService {
 		while(system) {
 		System.out.println("O que desejo pesquisar em GAME");
 		System.out.println("0 - Sair");
-		System.out.println("1 - Pesquisar Game por nome");
+		System.out.println("1 - Buscar Game por nome");
+		System.out.println("2 - Buscar Game por nome, produtora, ano de lancamento.");
 		
 		int action = sc.nextInt();
 		
 		switch (action) {
 		case 1:
 			pesquisaGameNome(sc);
+			break;
+		case 2:
+			buscaGameNomeProdutotraAnoLancamento(sc);
 			break;
 		default:
 			system = false;
@@ -42,11 +46,27 @@ public class RelatorioService {
 
 	private void pesquisaGameNome(Scanner sc) {
 		System.out.println("Qual nome deseja pesquisar");
-		String nome = sc.nextLine().toUpperCase();     
-        nome += sc.nextLine().toUpperCase();
+		String nome = sc.next();
+		
 		List<Game> list = gameRepository.findByNome(nome);
 		list.forEach(System.out::println);
 		
+	}
+	
+	
+	private void buscaGameNomeProdutotraAnoLancamento(Scanner sc) {
+		System.out.println("Qual nome de game deseja pesquisar");
+		String nome = sc.next();
+        
+        System.out.println("Qual produtora de game deseja pesquisar");
+		String produtora = sc.next();
+		
+		System.out.println("Qual ano de lancamento do game deseja pesquisar");
+		int ano = sc.nextInt();
+		
+		List<Game> list = gameRepository.findByNomeProdutoraAnoLancamentoGames(nome, produtora, ano);
+		list.forEach(System.out::println);
+        
 	}
 
 }
